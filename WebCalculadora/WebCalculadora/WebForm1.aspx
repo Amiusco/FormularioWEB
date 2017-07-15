0d677fa4b0947ebd219019d135969678e6b98e7a
Amiusco <amifranciscocortez@hotmail.com>
2017-07-15 13:35:03 -0500

    Modificado 15-07-2017
    Por fin quedo !!!

--- WebCalculadora/WebCalculadora/WebForm1.aspx
+++ WebCalculadora/WebCalculadora/WebForm1.aspx
@@ -4,9 +4,9 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <link href="bin/css/bootstrap.css" rel="stylesheet" />
-    <link href="bin/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="bin/css/bootstrap-theme.css" rel="stylesheet" />
+   <link href="Bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link href="Bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+    <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Primer Web Form</title>
 </head>
@@ -14,24 +14,51 @@
 
 <body>
     <form id="form1" runat="server">
+
         <div class="row">
-            <div class="col-md-4">
 
+              <div class="col-md-4"></div>
+            <div class="col-md-3" style="background-color: #01dfa5 ; box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75)" >
+           <table>
+       <tr>
+           <td style="padding-top:5%;">
+               <asp:Label ID="Label1" runat="server" Text="Nombre:"></asp:Label>
+           </td>
+           <td style="padding-top:5%;">
+               <asp:TextBox ID="TextBoxNombre" runat="server" CssClass="form-control"></asp:TextBox>
+           </td>
+       </tr>
+       
+       <tr>
+           <td style="padding-top:5%;">
+               <asp:Label ID="Label2" runat="server" Text="Apellido"></asp:Label>
+           </td>
+           <td style="padding-top:5%;">
+                <asp:TextBox ID="TextBoxApellido" runat="server" CssClass="form-control"></asp:TextBox>
+           </td>
+          
+       </tr>
+       <tr>
+           <td style="padding-top:5%;">
+               <asp:Label ID="Label3Correo" runat="server" Text="Correo"></asp:Label>
+           </td>
+           <td style="padding-top:5%;">
+<asp:TextBox ID="TextBoxEdad" runat="server" CssClass="form-control"></asp:TextBox>
+           </td>
+       </tr>
+         
+       <tr>
+           <td></td>
+           <td style="padding-top:5%;">
+               <asp:Button ID="ButtonEnviar" runat="server" Text="Enviar" CssClass="btn-success" OnClick="ButtonEnviar_Click" /> 
+                 
+           </td>
+       </tr>
 
-            </div>
-            <div class="col-md-4">
-
-
-            </div>
-            <div class="col-md-4">
-
-
-            </div>
-
-
-
-
+   </table>
+                <div class="col-md-5"></div>
         </div>
+            </div>
     </form>
 </body>
 </html>
--- WebCalculadora/WebCalculadora/WebForm1.aspx.designer.cs
+++ WebCalculadora/WebCalculadora/WebForm1.aspx.designer.cs
@@ -20,5 +20,68 @@ namespace WebCalculadora {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control TextBoxNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxNombre;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control TextBoxApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxApellido;
+        
+        /// <summary>
+        /// Control Label3Correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3Correo;
+        
+        /// <summary>
+        /// Control TextBoxEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxEdad;
+        
+        /// <summary>
+        /// Control ButtonEnviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonEnviar;
     }
 }
